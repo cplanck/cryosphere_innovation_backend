ea78e18e999f393a9e2ad150c9ef836732e6e920
Cameron Planck <cameronplanck@gmail.com>
2024-02-09 00:22:26 -0500

    upgrading data endpoint

--- real_time_data/endpoints.py
+++ real_time_data/endpoints.py
@@ -162,12 +162,15 @@ class SBDGmailPubSubEndpoint(viewsets.ViewSet):
 
         Written 8 Feb 2024
         """
-        print('IS THIS RUNNING AT ALL?')
-        print(request)
-        print(request.data)
-        print(type(request.data))
-        pub_sub_message_body = base64.b64decode(request.data['body'].encode("utf-8"))
-        print('PUB SUB MESSAGE BODY', pub_sub_message_body)
+        try:
+            print('IS THIS RUNNING AT ALL?')
+            print(request)
+            print(request.data)
+            print(type(request.data))
+            pub_sub_message_body = base64.b64decode(request.data['body'].encode("utf-8"))
+            print('PUB SUB MESSAGE BODY', pub_sub_message_body)
+        except Exception as e:
+            print(e)
 
         pub_sub_history_id = pub_sub_message_body['historyId']
         print('PUB SUB HISTORY ID', pub_sub_history_id)
