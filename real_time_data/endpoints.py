be02c69e1bf5baed83ec4f5cc4794378a1f87b58
Cameron Planck <cameronplanck@gmail.com>
2024-02-09 00:19:35 -0500

    upgrading data endpoint

--- real_time_data/endpoints.py
+++ real_time_data/endpoints.py
@@ -164,6 +164,8 @@ class SBDGmailPubSubEndpoint(viewsets.ViewSet):
         """
         print('IS THIS RUNNING AT ALL?')
         print(request)
+        print(request.data)
+        print(type(request.data))
         pub_sub_message_body = base64.b64decode(request.data['body'].encode("utf-8"))
         print('PUB SUB MESSAGE BODY', pub_sub_message_body)
 
