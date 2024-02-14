9d2b8406c95e586864624caaf1edade403c5dc17
Cameron Planck <cameronplanck@gmail.com>
2024-02-13 22:53:36 -0500

    updated notifications

--- notifications/notification_signals.py
+++ notifications/notification_signals.py
@@ -13,7 +13,7 @@ def admin_deployment_download_notification(sender, instance, created, **kwargs):
     if created:
          # Don't sent notifications to Cameron if Cameron downloaded
          if not instance.user.id == 1:
-            Notification.objects.create(for_user_id=1, from_user=instance.user, type='deployment_download', content={'deployment_id': instance.deployment.id, 'deployment_name': instance.deployment.name, 'from_user_details': {'first_name': instance.user.first_name, 'last_name': instance.user.last_name, 'avatar': instance.user.userprofile.get_avatar()}})
+            Notification.objects.create(for_user_id=1, from_user=instance.user, type='deployment_download', content={'deployment_id': instance.deployment.id, 'deployment_slug': instance.deployment.slug, 'deployment_name': instance.deployment.name, 'from_user_details': {'first_name': instance.user.first_name, 'last_name': instance.user.last_name, 'avatar': instance.user.userprofile.get_avatar()}})
 
 
 
--- stats/models.py
+++ stats/models.py
@@ -3,6 +3,6 @@ from instruments.models import Deployment
 from django.contrib.auth.models import User
 
 class DeploymentDownload(models.Model):
-    deployment = models.ForeignKey(Deployment, null=True, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
+    deployment = models.ForeignKey(Deployment, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, blank=True, on_delete=models.CASCADE)
     date_added = models.DateTimeField(auto_now_add=True, null=True)
