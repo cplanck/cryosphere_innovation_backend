928e6b2629c1f320dea98d3094b4bb95951620b3
Cameron Planck <cameronplanck@gmail.com>
2024-03-11 17:57:25 -0400

    updated public metadata endpoint

--- external_tests/test.py
+++ external_tests/test.py
@@ -1,7 +1,12 @@
 import requests
 
 
+# import requests
+# url = 'http://localhost:8000/public/deployments/9eed4974-a45b-4670-b807-a1e919543bb0'; 
+# response = requests.get(url, headers={'Authorization':'Bearer n6cQZOsEAzBN2NzN3m1regQXAFp2QUoA'}).json()
+# print(response)
+
 import requests
-url = 'http://localhost:8000/public/deployments/9eed4974-a45b-4670-b807-a1e919543bb0'; 
+url = 'https://api.cryosphereinnovation.com/public/deployments/'; 
 response = requests.get(url, headers={'Authorization':'Bearer n6cQZOsEAzBN2NzN3m1regQXAFp2QUoA'}).json()
 print(response)
